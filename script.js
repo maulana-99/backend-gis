39b20064a6422a46fa3574f27c457646947068c1
maulana-99 <ilhamdile05@gmail.com>
2024-08-12 11:39:54 +0700

    test9

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-// Inisialisasi peta
-var map = L.map('map').setView([0, 0], 2);
+// Fungsi untuk menghitung jarak menggunakan rumus Haversine
+function haversineDistance(coords1, coords2) {
+    function toRad(x) {
+        return x * Math.PI / 180;
+    }
 
-// Menambahkan tile layer dari OpenStreetMap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    var lat1 = coords1[0];
+    var lon1 = coords1[1];
+    var lat2 = coords2[0];
+    var lon2 = coords2[1];
+
+    var R = 6371; // Radius bumi dalam kilometer
+    var x1 = lat2 - lat1;
+    var dLat = toRad(x1);
+    var x2 = lon2 - lon1;
+    var dLon = toRad(x2)
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+
+    return d; // Jarak dalam kilometer
+}
 
 // Fungsi untuk memuat data McDonald's dari API backend
-function loadMcDonalds() {
+function loadMcDonalds(userCoords) {
     fetch('http://localhost:8080/mcdonalds')
         .then(response => response.json())
         .then(data => {
             data.data.mcdonalds.forEach(mcd => {
-                L.marker([mcd.latitude, mcd.longitude])
+                var mcdCoords = [mcd.latitude, mcd.longitude];
+                var distance = haversineDistance(userCoords, mcdCoords).toFixed(2); // Hasil dalam km
+
+                L.marker(mcdCoords)
                     .addTo(map)
-                    .bindPopup(`<b>${mcd.name}</b>`);
+                    .bindPopup(`<b>${mcd.name}</b><br>Distance: ${distance} km`);
             });
         })
         .catch(error => console.error('Error loading McDonalds:', error));
@@ -26,11 +46,16 @@ function locateUser() {
         navigator.geolocation.getCurrentPosition(position => {
             var latitude = position.coords.latitude;
             var longitude = position.coords.longitude;
-            map.setView([latitude, longitude], 15);
-            L.marker([latitude, longitude])
+            var userCoords = [latitude, longitude];
+
+            map.setView(userCoords, 15);
+            L.marker(userCoords)
                 .addTo(map)
                 .bindPopup("You are here")
                 .openPopup();
+
+            // Memuat data McDonald's setelah lokasi pengguna ditemukan
+            loadMcDonalds(userCoords);
         }, () => {
             alert("Unable to retrieve your location.");
         });
@@ -39,5 +64,13 @@ function locateUser() {
     }
 }
 
-// Memuat data McDonald's saat halaman dimuat
-loadMcDonalds();
+// Inisialisasi peta
+var map = L.map('map').setView([0, 0], 2);
+
+// Menambahkan tile layer dari OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Menjalankan locateUser saat halaman dimuat
+locateUser();
